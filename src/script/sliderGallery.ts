5cc8ca50a58d1f084aea31f266ce80d58f12a30f
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-04-11 23:31:54 +0300

    add min form notExit

--- src/script/sliderGallery.ts
+++ src/script/sliderGallery.ts
@@ -3,18 +3,52 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 
 const sliderGallery = () => {
-  new Swiper("#sliderGallery", {
-    modules: [Navigation, Pagination],
-    slidesPerView: 1,
-    navigation: {
-      prevEl: ".prevGallery",
-      nextEl: ".nextGallery",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  const sliderGalleryElements =
+    document.querySelectorAll<HTMLElement>(".sliderGallery");
+
+  sliderGalleryElements.forEach((sliderGallery) => {
+    const idSlider = sliderGallery.id;
+
+    new Swiper(`#slider${idSlider}`, {
+      modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      navigation: {
+        prevEl: `.prev${idSlider}`,
+        nextEl: `.next${idSlider}`,
+      },
+      pagination: {
+        el: `.swiper-pagination-${idSlider}`,
+        clickable: true,
+      },
+    });
   });
 };
-
 sliderGallery();
+
+const selectGallery = () => {
+  const selectGallerysButtonElements =
+    document.querySelectorAll<HTMLButtonElement>(".select-gallerys-button");
+
+  selectGallerysButtonElements.forEach((selectGallerysButton) => {
+    selectGallerysButton.onclick = () => {
+      const selectButtonGallery = selectGallerysButton.dataset[
+        "gallery"
+      ] as string;
+      const activeGallery = document.getElementById(selectButtonGallery);
+      const allGallery =
+        document.querySelectorAll<HTMLElement>(".sliderGallery");
+
+      selectGallerysButtonElements.forEach((el) => {
+        el.classList.remove("active");
+      });
+
+      allGallery.forEach((el) => {
+        el.classList.remove("active");
+      });
+
+      selectGallerysButton.classList.add("active");
+      activeGallery?.classList.add("active");
+    };
+  });
+};
+selectGallery();
